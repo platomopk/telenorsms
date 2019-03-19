8bde38732f46ddb79f63713ff52aca6c2bfbfe44
Khyzar Kapoor <khyzar@protonmail.com>
2019-03-19 20:54:21 +0500

    after static add

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.use(function(req, res, next) {
   });
 
 //static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public'),{maxAge:'1d'}));
 
 //get form data incoming request
 app.use(bodyParser.json());
